63806968b1aa2c15b84f5fe48b0839a59c69e648
Tran Tien Manh <trantienmanh@luvina.net>
2025-03-24 17:32:37 +0700

    Update Swagger and start test

--- src/main/java/com/social_luvina/social_dev8/helpers/JwtAuthFilter.java
+++ src/main/java/com/social_luvina/social_dev8/helpers/JwtAuthFilter.java
@@ -44,7 +44,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     String path = request.getRequestURI();
     return path.startsWith("/social/auth/login") || path.startsWith("/social/auth/register")
     || path.startsWith("/social/auth/forgetpassword") || path.startsWith("/social/auth/change_password")
-    || path.startsWith("/social/auth/profile");
+    || path.startsWith("/social/auth/profile") || path.startsWith("/swagger-ui/") || path.startsWith("/v3/api-docs") 
+    || path.startsWith("/swagger-resources")  || path.startsWith("/webjars/");
   }
 
   @Override
